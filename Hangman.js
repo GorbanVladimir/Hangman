ad74d9654368f35162916dc8f192a8d12fa23e93
Vladimir <gorban469@gmail.com>
2022-03-24 14:55:47 +0200

    Fix bug with alerts and rename functions

--- Hangman.js
+++ Hangman.js
@@ -1,7 +1,9 @@
 let specialWord = ''
-let input = document.getElementById('addWord')
-let check = document.getElementById('check')
+let input = document.getElementById('input')
 let sectionBody = document.getElementById('sectionBody')
+let cardBody = document.getElementById('cardBody')
+let image = document.getElementById('image')
+let alerts = document.getElementById('alert')
 let searchedLetters = ''
 
 function reload () {
@@ -9,93 +11,101 @@ function reload () {
 }
 
 function addRestartButton () {
-    document.getElementById('cardBody').innerHTML = '<button type="button" class="btn btn-secondary" onclick="reload()">Restart the game</button>'
+    cardBody.innerHTML = '<button type="button" class="btn btn-secondary" onclick="reload()">Restart the game</button>'
 }
 
-function checkLettersearched (currentLetter) {
-    return searchedLetters.indexOf(currentLetter)
+function checkLetterSearched (currentLetter) {
+    if (searchedLetters.indexOf(currentLetter) >= 0) {
+        alerts.innerHTML = `<div class="alert alert-dark" role="alert">'${currentLetter}' has already been introduced</div>`
+        input.value = ''
+        return 0
+    }
+    return -1
 }
 
 function isALetter (letter) {
     return letter.toUpperCase() !== letter.toLowerCase()
 }
 
-function addWord () {
-    specialWord = input.value.toUpperCase()
-    document.getElementById('inputAndButtonDelete').innerHTML = ''
+function isAWord (word) {
+    for (let i of word) {
+        if (i.toUpperCase() === i.toLowerCase()) {
+            return false
+        }
+    }
+    return true
+}
+
+function replaceInput () {
+    cardBody.innerHTML = '            <p class="card-text">Enter a letter to guess the word</p>\n' +
+        '            <label for="input"></label><input type="text" id="input" placeholder="Type a letter">\n' +
+        '            <input type="button" value="OK"  id="inputButton" onclick="checkTheLetter()">'
+}
+
+function clearSectionBody () {
     sectionBody.innerHTML = ''
-    document.getElementById('image').innerHTML = '<img src="Images/0.jpg" alt="HangmanSteps">'
-    let lgWord = specialWord.length
-    for (let i = 0; i < lgWord; ++i) {
+}
+
+function createSection () {
+    for (let i = 0; i < specialWord.length; ++i) {
         sectionBody.innerHTML += `<th id="letterNumber ${i}"></th>`
     }
 }
 
-let counterChance = 1
-let counterSucces = 0
-
-function checkTheLetter () {
-    let inputLetter = check.value.toUpperCase()
-    if (specialWord.length) {
-        if (isALetter(inputLetter)) {
-            if (inputLetter.length === 1) {
-                let indexOfTheLetter = specialWord.indexOf(inputLetter)
-                if (indexOfTheLetter >= 0 && checkLettersearched(inputLetter) < 0) {
-                    searchedLetters += inputLetter
-                    document.getElementById('alert').innerHTML = '<div class="alert alert-success" role="alert">\n' +
-                        '  RIGHT!\n' +
-                        '</div>'
-                    while (indexOfTheLetter >= 0) {
-                        ++counterSucces
-                        let section = document.getElementById(`letterNumber ${indexOfTheLetter}`)
-                        section.innerHTML = `${specialWord.charAt(indexOfTheLetter)}`
-                        indexOfTheLetter = specialWord.indexOf(inputLetter, ++indexOfTheLetter)
-                        check.value = ''
-                        if (counterSucces === specialWord.length) {
-                            alert('YOU GUESSED IT!')
-                            addRestartButton()
-                        }
-                    }
-                } else if (indexOfTheLetter < 0 && counterChance < 10) {
-                    document.getElementById('alert').innerHTML = `<div class="alert alert-warning" role="alert">\n` +
-                        `  Is incorrect but you still have&nbsp;${10 - counterChance}&nbsp;chances!\n` +
-                        `</div>`
-                    let img = document.getElementById('image')
-                    img.innerHTML = `<img src="Images/${counterChance}.jpg" alt="HangmanSteps">`
-                    ++counterChance
-                    check.value = ''
-                } else if (indexOfTheLetter < 0 && counterChance >= 10) {
-                    console.log('counterChance' + counterChance)
-                    document.getElementById('image').innerHTML = `<img src="Images/${counterChance}.jpg" alt="HangmanSteps">`
-                    alert('MAYBE NEXT TIME!')
-                    document.getElementById('alert').innerHTML = ''
-                    sectionBody.innerHTML = `<p>THE WORD YOU MISSED : ${specialWord}</p>`
-                    addRestartButton()
-                }
-            } else {
-                alert('Type only one letter please')
-                check.value = ''
-            }
-        } else {
-            alert('It is not a letter!')
-            check.value = ''
-        }
-    } else {
-        alert('First enter a word :D')
-        check.value = ''
+function wordInitiation () {
+    specialWord = input.value.toUpperCase()
+    if (specialWord !== '' && isAWord(specialWord)) {
+        replaceInput()
+        clearSectionBody()
+        createSection()
+        image.innerHTML = '<img src="Images/0.jpg" alt="HangmanSteps">'
+    } else if (specialWord === '') {
+        alerts.innerHTML = '<div class="alert alert-warning" role="alert">First type a word :D!</div>'
+    } else if (!isAWord(specialWord)) {
+        alerts.innerHTML = '<div class="alert alert-warning" role="alert">Is not a word!</div>'
+        input.value = ''
     }
 }
 
-check.addEventListener('keyup', function(event) {
-    event.preventDefault();
-    if (event.keyCode === 13) {
-        document.getElementById('checkButton').click();
-    }
-});
+let counterChance = 1
+let counterSuccess = 0
 
-input.addEventListener('keyup', function(event) {
-    event.preventDefault();
-    if (event.keyCode === 13) {
-        document.getElementById('inputButton').click();
+function checkTheLetter () {
+    input = document.getElementById('input')
+    let inputLetter = input.value.toUpperCase()
+    if (isALetter(inputLetter) && inputLetter.length === 1) {
+        let indexOfTheLetter = specialWord.indexOf(inputLetter)
+        if (indexOfTheLetter >= 0 && checkLetterSearched(inputLetter) < 0) {
+            searchedLetters += inputLetter
+            alerts.innerHTML = '<div class="alert alert-info" role="alert">RIGHT!\n</div>'
+            do {
+                ++counterSuccess
+                let section = document.getElementById(`letterNumber ${indexOfTheLetter}`)
+                section.innerHTML = `${specialWord.charAt(indexOfTheLetter)}`
+                indexOfTheLetter = specialWord.indexOf(inputLetter, ++indexOfTheLetter)
+                input.value = ''
+                if (counterSuccess === specialWord.length) {
+                    alerts.innerHTML = '<div class="alert alert-success" role="alert">YOU GUESSED IT!</div>'
+                    addRestartButton()
+                }
+            } while (indexOfTheLetter >= 0)
+        } else if (indexOfTheLetter < 0 && counterChance < 10) {
+            alerts.innerHTML = `<div class="alert alert-warning" role="alert">Is incorrect, but you still have&nbsp;${10 - counterChance}&nbsp;chances!</div>`
+            let img = document.getElementById('image')
+            img.innerHTML = `<img src="Images/${counterChance}.jpg" alt="HangmanSteps">`
+            ++counterChance
+            input.value = ''
+        } else if (indexOfTheLetter < 0 && counterChance >= 10) {
+            document.getElementById('image').innerHTML = `<img src="Images/${counterChance}.jpg" alt="HangmanSteps">`
+            alerts.innerHTML = '<div class="alert alert-danger" role="alert">MAYBE NEXT TIME!</div>'
+            sectionBody.innerHTML = `<p>THE WORD YOU MISSED : ${specialWord}</p>`
+            addRestartButton()
+        }
+    } else if (!isALetter(inputLetter)) {
+        alerts.innerHTML = '<div class="alert alert-warning" role="alert">It is not a letter!</div>'
+        input.value = ''
+    } else if (inputLetter.length !== 1) {
+        alerts.innerHTML = '<div class="alert alert-warning" role="alert">Type only one letter please</div>'
+        input.value = ''
     }
-});
+}
